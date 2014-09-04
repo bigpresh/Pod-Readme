b27253fc06b60c7d9fe0499af1b766be225b8ce8
Robert Rothenberg <rrwo@cpan.org>
2014-09-04 05:13:31 +0100

    tmp dev script tweaks [ci skip]

--- tmp/filter.pl
+++ tmp/filter.pl
@@ -20,6 +20,9 @@ use IO::File;
 
 my $io = IO::File->new($0, 'r');
 
-my $prf = Pod::Readme::Filter->new( input_fh => $io );
+my $prf = Pod::Readme::Filter->new( input_file => $0 );
+
+use Data::Printer;
+p $prf->input_file;
 
 $prf->filter_file;
